45df1f8c4dc001f2edf04cbd8bf7be3a265e5e9a
Chris Nattress <chris.nattress@progleasing.com>
2021-01-30 13:09:24 -0700

    Answer #2

--- Interview.Business.Tests/CustomerServiceTests.cs
+++ Interview.Business.Tests/CustomerServiceTests.cs
@@ -1,4 +1,6 @@
-﻿using Interview.Business.Services;
+﻿using Interview.Business.Repositories;
+using Interview.Business.Services;
+using Interview.Model.Types;
 using Moq;
 using NUnit.Framework;
 
@@ -12,13 +14,32 @@ namespace Interview.Business.Tests
         [SetUp]
         public void Initialize()
         {
-            _customerService = new CustomerService();
+            var mockCustomerRepository = new Mock<ICustomerRepository>();
+
+            mockCustomerRepository.Setup(_ => _.GetCustomer(It.IsAny<string>())).Returns(new Customer
+            {
+                Age = 30,
+                FirstName = "John",
+                Gender = "M",
+                Id = "1",
+                LastName = "Doe"
+            });
+
+            _customerService = new CustomerService(mockCustomerRepository.Object);
         }
 
         [Test]
         public void GetCustomers_WithCustomerId_ReturnsCustomer()
         {
-            Assert.Pass();
+            // Arrange
+            var id = "1";
+
+            // Act
+            var customer = _customerService.GetCustomer(id);
+
+            // Assert
+            Assert.IsNotNull(customer);
+            Assert.IsInstanceOf<Customer>(customer);
         }
     }
 }
